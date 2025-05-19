f979dafa1303d3fb8f1cdced8e0c9cc5b73e5efc
Joao <joaorsramos@Hotmail.com>
2025-05-19 13:31:08 -0300

    initial config project

---
+++ agent-logistic/src/main/java/joaorsramos09/com/agent_logistic/AgentLogisticApplication.java
@@ -0,0 +1,13 @@
+package joaorsramos09.com.agent_logistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentLogisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentLogisticApplication.class, args);
+	}
+
+}
---
+++ agent-logistic/src/test/java/joaorsramos09/com/agent_logistic/AgentLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package joaorsramos09.com.agent_logistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentLogisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
